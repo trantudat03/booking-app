e9244d18670d58d6693ca3c71294e384bdadce57
trantudat <trantudat03@gmail.com>
2023-11-07 02:01:13 +0700

    Commit sau khi lam xong post user len mongodb

---
+++ api/Index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const cors = require('cors');
+const { default: mongoose } = require('mongoose');
+const bcrypt = require('bcryptjs')
+
+const User = require('./models/User')
+require('dotenv').config();
+
+
+const app = express();
+
+const bcryptSalt = bcrypt.genSaltSync(10);
+app.use(express.json())
+app.use(cors({
+    credentials: true,
+    origin: 'http://127.0.0.1:5173',
+
+}));
+
+
+mongoose.connect(process.env.MONGO_URL);
+
+app.get('/test', (req, res) => {
+    res.json('test okkk');
+});
+
+//IGX5lqymKsuOK5Y4
+
+app.post('/register', async (req, res) => {
+    const {name, email, password} = req.body;
+    const userDoc = await User.create({
+        name,
+        email,
+        password:bcrypt.hashSync(password, bcryptSalt),
+    });
+    res.json(userDoc);
+})
+
+app.listen(4000);
